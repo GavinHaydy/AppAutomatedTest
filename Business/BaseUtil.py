ecaea138be517c77b9358d72b1d771f8dfacf7d1
bugpz <13558622779@163.com>
2021-05-13 12:23:09 +0800

    fix：增加函数二次封装

--- Business/BaseUtil.py
+++ Business/BaseUtil.py
@@ -1,4 +1,3 @@
-from appium import webdriver
 
 
 class BasePage(object):
@@ -29,3 +28,17 @@ class BasePage(object):
     def by_css(self, css):
         return self.driver.find_element_by_css_selector(css)
 
+    def get_witch(self):    # 获取屏幕宽
+        return self.driver.get_window_size()['witch']
+
+    def get_height(self):   # 获取屏幕高
+        return self.driver.get_window_size()['height']
+
+    def get_url(self):  # 获取当前页的url
+        return self.driver.current_url()
+
+    def get_source(self):   # 获取当前页的源
+        return self.driver.page_source()
+
+    def screenshot(self, filename):    # 截图
+        return self.driver.get_screenshot_as_file(filename)
